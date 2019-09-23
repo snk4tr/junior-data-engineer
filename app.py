2217ca995d0739b5916caf5655a852303584c180
Sergey Kastryulin <snk4tr@gmail.com>
2019-09-23 19:45:45 +0300

    docs(app): add description of entry point parameters

--- app.py
+++ app.py
@@ -9,4 +9,7 @@ def hello_world():
 
 
 if __name__ == '__main__':
+
+    # Note that localhost (127.0.0.1) is used by default.
+    # This configuration will not let to successfully map default port (5000) with docker.
     app.run(debug=True, host='0.0.0.0')

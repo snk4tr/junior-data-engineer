64c79e077bc459bd80c66c4b8a85f79c4c7df99f
Sergey Kastryulin <snk4tr@gmail.com>
2019-09-25 20:48:29 +0300

    fix(util): get rid of annoying yaml warning

--- junior_data_engineer/util/initializers.py
+++ junior_data_engineer/util/initializers.py
@@ -3,6 +3,6 @@ import yaml
 
 def load_config(path: str) -> dict:
     with open(path, 'r') as f:
-        config = yaml.load(f)
+        config = yaml.load(f, Loader=yaml.FullLoader)
 
     return config
